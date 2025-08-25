8b4abd53bcf3d9d7e53c75a3f2c1e03d85f88148
KDS <2366454011@qq.com>
2025-08-25 19:32:44 +0800

    最初搭建项目

---
+++ shopping_goods_service/src/main/java/com/baizhanshopping/shopping_goods_service/ShoppingGoodsServiceApplication.java
@@ -0,0 +1,22 @@
+package com.baizhanshopping.shopping_goods_service;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+//注册nacos注解
+@EnableDiscoveryClient
+//开启服务注册发现
+@EnableDubbo
+//动态刷新配置文件
+@RefreshScope
+public class ShoppingGoodsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingGoodsServiceApplication.class, args);
+    }
+
+}
